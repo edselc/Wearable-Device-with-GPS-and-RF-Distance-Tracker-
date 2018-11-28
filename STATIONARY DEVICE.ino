66ab2fa3d4e49a1245e688ce0e4b71f6c8f368ef
edselc <edselcalanoga14@gmail.com>
2018-11-28 20:32:26 +0800

    Add files via upload

---
+++ STATIONARY DEVICE.ino
@@ -0,0 +1,93 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <RH_ASK.h>
+#include <SPI.h>
+
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+RH_ASK rf_driver;
+int led = 13;
+int buzzer = 12;
+
+void setup()
+{
+  
+  Serial.begin(9600);
+  //Initialize ASK object
+  rf_driver.init();
+  lcd.begin();
+  lcd.backlight();
+  pinMode(led, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+    
+  Serial.println("Setting up");  
+  lcd.setCursor(7,0);
+  lcd.print("SYSTEM");
+  lcd.setCursor(4,1);
+  lcd.print("INITIALIZING");
+  lcd.setCursor(7,2);
+  lcd.print("PLEASE");
+  lcd.setCursor(8,3);
+  lcd.print("WAIT"); 
+  delay(10000); 000
+  digitalWrite(led, LOW);
+  digitalWrite(buzzer, LOW);
+
+  
+}
+
+void loop()
+{
+  recieve_data();
+}
+
+void recieve_data()
+{
+  uint8_t buf[10];
+  uint8_t buflen = sizeof(buf);
+  if (rf_driver.waitAvailableTimeout(4000))
+  {    
+   if (rf_driver.recv(buf, &buflen))
+   {
+     Serial.print("Message Receivedd: ");
+     Serial.println((char*)buf);
+     lcd.clear();
+     lcd.setCursor(6,0);
+     lcd.print("IN RANGE");
+     lcd.setCursor(1,2);
+     lcd.print("WEARABLE  TRACKING");
+     lcd.setCursor(7,3);
+     lcd.print("DEVICE");
+     digitalWrite(led, LOW);
+   }  
+  }
+  
+  else
+  {
+      Serial.println("No message Receive");
+      lcd.clear();
+      lcd.setCursor(4,0);
+      lcd.print("NOT IN RANGE");
+      lcd.setCursor(6,1);
+      lcd.print("Send SMS");
+      lcd.setCursor(1,2);
+      lcd.print("WEARABLE  TRACKING");
+      lcd.setCursor(7,3);
+       lcd.print("DEVICE");
+      led_alarm();
+      buzzer_alarm();
+      
+  }
+}
+
+void led_alarm()
+{
+  digitalWrite(led, HIGH);
+  delay(3000);
+  digitalWrite(led, LOW);
+  delay(400);
+}
+void buzzer_alarm()
+{
+  digitalWrite(buzzer, HIGH);
+}
---
+++ WEARABLE TRACKING DEVICE.ino
@@ -0,0 +1,161 @@
+
+#include <RH_ASK.h>
+#include <SPI.h>
+#include <SoftwareSerial.h>
+SoftwareSerial sim808(3, 2);
+RH_ASK driver;
+char phone_no[] = " "; // insert the phone number of the receiver inside the quotations
+String data[5];
+#define DEBUG true
+String state, timegps, latitude, longitude, textMessage;
+const int led = 13;
+void setup()
+{
+sim808.begin(9600);
+Serial.begin(9600);
+delay(50);
+sim808.print("AT\r");
+delay(100);
+sim808.print("ATE0\r");
+delay(400);
+sim808.print("AT+CMGF=1\r");
+delay(400);
+sim808.print("AT+CMGD=1,4\r");
+delay(400);
+sim808.print("AT+CNMI=1,2,0,0,0\r");
+delay(100);
+sendData("AT+CGNSPWR=1", 1000, DEBUG);
+delay(50);
+sendData("AT+CGNSSEQ=RMC", 1000, DEBUG);
+
+
+sendTabData("AT+CGNSINF", 1000, DEBUG);
+
+
+delay(150);
+pinMode(led, OUTPUT);
+digitalWrite(led, LOW);
+if (!driver.init())
+Serial.println("rf driver init failed");
+}
+void loop()
+{
+transmitter();
+
+sim808.print("AT+CNMI=1,2,0,0,0\r");
+delay(100);
+if (sim808.available() > 0)
+  {
+    textMessage = sim808.readString();
+    Serial.print(textMessage);
+    delay(10);
+  }
+if (textMessage.indexOf("GPS") >= 0)
+  {
+    digitalWrite(led, HIGH);
+    Serial.println("GPS");
+   Send_SMS(); 
+
+  }
+}
+void transmitter()
+{
+  const char *msg = "0123456789";
+  driver.send((uint8_t *)msg, strlen(msg));
+  driver.waitPacketSent();
+  
+  
+  sendTabData("AT+CGNSINF", 1000, DEBUG);
+  
+  
+  delay(200);
+}
+
+void Send_SMS()
+{
+      sendTabData("AT+CGNSINF", 1000, DEBUG);
+    if (state != 0)
+      {
+        Serial.println("State :" + state);
+        Serial.println("Time :" + timegps);
+        Serial.println("Latitude :" + latitude);
+        Serial.println("Longitude :" + longitude);
+        sim808.print("AT+CMGS=\"");
+    
+        sim808.print(phone_no);
+        sim808.println("\"");
+        delay(300);
+        sim808.print("http://maps.google.com/maps?q=");
+        sim808.print(latitude);
+        sim808.print(",");
+        sim808.print (longitude);
+        delay(200);
+        sim808.println((char)26);
+        delay(200);
+        sim808.println();
+        delay(20000);
+        sim808.flush();
+      }
+        else
+        {
+           Serial.println("GPS Initializing…");
+        }
+}
+
+void sendTabData(String command , const int timeout , boolean debug) 
+{
+  sim808.println(command);
+  long int time = millis();
+  int i = 0;
+  while ((time + timeout) > millis())
+  {
+      while (sim808.available())
+    {
+        char c = sim808.read();
+        if (c != ',')
+      {
+        data[i] += c;
+        delay(100);
+      }
+        else
+      {
+        i++;
+      }
+        if (i == 5)
+      {
+        delay(100);
+        goto exitL;
+      }
+    }
+  }
+  exitL:
+  if (debug)
+  {
+  state = data[1];
+  timegps = data[2];
+  latitude = data[3];
+  longitude = data[4];
+  
+  }
+}
+
+String sendData (String command , const int timeout , boolean debug)
+{
+  String response = "";
+  sim808.println(command);
+  long int time = millis();
+  int i = 0;
+  while ( (time + timeout ) > millis())
+  {
+      while (sim808.available())
+    {
+      char c = sim808.read();
+      response += c;
+    }
+  }
+    if (debug)
+  {
+    Serial.print(response);
+  }
+  return response;
+}
